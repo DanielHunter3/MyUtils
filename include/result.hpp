2497caa7a1b3e0f909b70104e7fccda0b6a71750
DanielHunter3 <danielhunter19991506@gmail.com>
2025-04-02 15:41:52 +0400

    second

--- include/result.hpp
+++ include/result.hpp
@@ -13,6 +13,26 @@ class ResultException : public std::exception {
     const char* message_;
 };
 
+template <typename E>
+class StructError {
+  public:
+    StructError(const E&& type, const std::string&& message)
+      : m_type(std::move(type)), m_message(std::move(message)) {}
+    StructError(const E&& type)
+      : m_type(std::move(type)), m_message("No information") {}
+
+    E type() const noexcept {
+      return m_type;
+    }
+    const std::string& message() const noexcept {
+      return m_message;
+    }
+
+  private:
+    E m_type;
+    std::string m_message;
+};
+
 // T - type of value
 // E - type of error
 // If T == E => UB
@@ -29,18 +49,18 @@ class Result {
     Result(T&& value) 
       : m_value(std::move(value)) {}
 
-    Result(E&& error)
+    Result(const StructError<E>&& error)
       : m_value(std::move(error)) {}
 
     constexpr bool is_error() const noexcept {
-      return std::holds_alternative<E>(m_value);
+      return std::holds_alternative<StructError<E>>(m_value);
     }
     constexpr bool is_ok() const noexcept { 
       return std::holds_alternative<T>(m_value);
     }
 
     const T& operator*() const {
-      if (std::holds_alternative<E>(m_value)) {
+      if (std::holds_alternative<StructError<E>>(m_value)) {
         throw ResultException(error().message());
       }
       return std::get<T>(m_value);
@@ -50,22 +70,22 @@ class Result {
     Unwraps the Result and returns the contained value.
     If the Result is not an error, it will throw the exception.
     */
-    const E error() const {
+    const StructError<E> error() const {
       if (std::holds_alternative<T>(m_value)) {
           throw ResultException("Object is not error");
       }
-      return std::get<E>(m_value);
+      return std::get<StructError<E>>(m_value);
     }
 
     const T unwrap() const {
-      if (std::holds_alternative<E>(m_value)) {
+      if (std::holds_alternative<StructError<E>>(m_value)) {
           throw ResultException(error().message());
       }
       return std::get<T>(m_value);
     }
 
     const T except(std::string&& message) const {
-      if (std::holds_alternative<E>(m_value)) {
+      if (std::holds_alternative<StructError<E>>(m_value)) {
           throw ResultException(message);
       }
       return std::get<T>(m_value);
@@ -79,5 +99,5 @@ class Result {
   }
 
   private:
-    std::variant<T, E> m_value;
+    std::variant<T, StructError<E>> m_value;
 };
